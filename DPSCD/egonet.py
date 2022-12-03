5a874806faa3c5b7c8c9eb81a44f34b875c2351c
andybi7676 <andybi7676@gmail.com>
2022-12-03 15:38:20 +0800

    fix some bugs

--- DPSCD/egonet.py
+++ DPSCD/egonet.py
@@ -25,7 +25,7 @@ class Egonet():
         self.sigma = 0.25
         self.d_c = 0.1
         self.egonet_user_ids = []
-        with open(egonets_fp, 'r') as er:
+        with open(egonet_fpath, 'r') as er:
             lines = er.readlines()
             for line in tqdm(lines):
                 contents = line.strip('\r\n ').split(':')
@@ -33,9 +33,9 @@ class Egonet():
                 friends_ids = [int(fr) for fr in contents[1].strip('\r\n ').split()]
                 user = self.users[user_id]
                 for fr_id in friends_ids:
-                    user.add_neighbor_to_dict(fr_id)
-                user.stabilized()
-                self.egonet_user_ids.append(user)
+                    user.add_neighbor_to_dict(self.users[fr_id])
+                user.stabilize()
+                self.egonet_user_ids.append(user_id)
         self.egonet_user_ids.sort()
         self.size = len(self.egonet_user_ids)
         self.create_iv_pairs()
@@ -48,13 +48,14 @@ class Egonet():
         for id_i in self.egonet_user_ids:
             for id_j in self.egonet_user_ids:
                 if id_i != id_j:
-                    self.iv_pairs.append((id_i, id_j))
+                    self.valid_iv_pairs.append((id_i, id_j))
+        print((1, 2) in self.valid_iv_pairs)
     
     def log(self, log_str: str):
         print(log_str)
 
     def simN(self, user_i, user_v):
-        salton_score = len(user_i & user_v) / math.sqrt(len(user_i) * len(user_v))
+        salton_score = len(user_i & user_v) / max(1, math.sqrt(len(user_i) * len(user_v)))
         return salton_score
     
     def simP(self, user_i, user_v):
@@ -69,10 +70,10 @@ class Egonet():
         for id_i, id_v in self.valid_iv_pairs:
             user_i = self.users[id_i]
             user_v = self.users[id_v]
-        simP = self.simP(user_i, user_v)
-        simN = self.simN(user_i, user_v)
-        dis_i_v = 1 / (self.alpha*simP + (1-self.alpha)*simN)
-        self.D[(id_i, id_v)] = dis_i_v
+            simP = self.simP(user_i, user_v)
+            simN = self.simN(user_i, user_v)
+            dis_i_v = 1 / max((self.alpha*simP + (1-self.alpha)*simN), 1e-5)
+            self.D[(id_i, id_v)] = dis_i_v
     
     def calculate_local_density(self):
         # store local density in 'user' also
@@ -94,13 +95,26 @@ class Egonet():
         user_rhos_npy = np.array([self.Rho[user_id] for user_id in self.egonet_user_ids])
         max_user_id = user_ids_npy[np.argmax(user_rhos_npy)]
         for id_i, rho_i in zip(user_ids_npy, user_rhos_npy):
-            v_filter = users_rhos_npy > rho_i # implicitly filter out id_i
-            v_candidates = user_ids_npy[v_filter]
             if id_i == max_user_id:
-                self.delta[id_i] = max(self.get_distance[(id_i, id_v)] for id_v in v_candidates)
+                v_filter = user_ids_npy != id_i
+                metric = "max"
             else:
-                self.delta[id_i] = min(self.get_distance[(id_i, id_v)] for id_v in v_candidates)
+                v_filter = user_rhos_npy > rho_i # implicitly filter out id_i
+                metric = "min"
+            v_candidates = user_ids_npy[v_filter]
+            self.delta[id_i] = eval(metric)([self.get_distance(id_i, id_v) for id_v in v_candidates])
     
-        
+
+if __name__ == "__main__":
+    featurelist_fp = '/home/andybi7676/Desktop/ds2022_finalProj/featureList.txt'
+    feature_fp = '/home/andybi7676/Desktop/ds2022_finalProj/features.txt'
+    cache_fp = '.cache/users.pkl'
+    
+    users = Users(feature_fp, featurelist_fp) 
+    Egonet.register_users(users)
+    ego_0_fpath = '/home/andybi7676/Desktop/ds2022_finalProj/egonets/0.egonet'
+    ego_0 = Egonet(ego_0_fpath)  
+    print(ego_0.Rho)
+    print(ego_0.delta) 
 
     
